04b09c8d3cc33b1e7932c69efeb7b3cd31951c87
shahrukh-navtech <shahrukh.k@navtech.io>
2019-11-19 17:16:56 +0530

    updated

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,12 +1,4 @@
 class ArticlePolicy < ApplicationPolicy
-  def index?
-    # user.admin? or not record.published?
-    # binding.pry
-    # @article.user == self.user
-    @articles = @article.each { |article| article.user == self.user }
-    # binding.pry
-    # true
-  end
 
   def edit?
   	@article.user == self.user ? true : false
